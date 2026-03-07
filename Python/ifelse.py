e87acad4db8256a927e5e6a4822ad314bfd1b159
codeliner123 <155770564+codeliner123@users.noreply.github.com>
2026-03-07 23:34:05 +0530

    Add files via upload

---
+++ Python/ifelse.py
@@ -0,0 +1,8 @@
+number = 11
+secondnum = 109
+if number ==15: 
+    print("This number will be fifteen")
+elif secondnum == 10:
+    print("This number will be ten")
+else:
+    print("Ayo what the helly")
